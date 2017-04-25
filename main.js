bd8a804003e24aadcc34cae264a6110d59bae81f
Luke Henwood <luke8h@gmail.com>
2017-04-24 19:34:04 -0500

    used split,reverse,join to reverse the string

---
+++ main.js
@@ -0,0 +1,16 @@
+
+function reverseString(str) {
+return str.split('').reverse().join('');
+}
+
+console.log(reverseString("hello"));
+
+
+
+// function foo(str) {
+//     ....
+// }
+//
+// var str = "some text";
+//
+// foo(str);
